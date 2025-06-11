fb73bf881f0d60a4d6cb01ec628f2a668bc3fdbc
vivek0455 <pandey0455469.gmail.com>
2025-06-11 19:00:41 +0530

    liner search

--- BinarySearch/Basic.java
+++ BinarySearch/Basic.java
@@ -1,7 +1,57 @@
 package BinarySearch;
 
+import java.util.Scanner;
+
 public class Basic {
-    public static void main(String[] args) {
+
+    public static void print(int[] arr, int n){
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    //liner search
+
+    public static int findElement(int[] arr, int n, int target){
+
+        for(int i=0;i<n;i++){
+            if(arr[i] == target)return i;
+        }
         
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        int n;
+        System.out.println("Enter the size of the array");
+        n =  sc.nextInt();
+
+        int[] arr =  new int[n];
+
+        for(int i=0;i<n;i++){
+            int temp =  sc.nextInt();
+            arr[i] = temp;
+        }
+
+        System.out.println("The Given array is : ");
+        print(arr, n);
+        
+
+        int target;
+        System.out.println("Enter the target element : ");
+        target = sc.nextInt();
+
+
+        int index = findElement(arr, n, target);
+        if(index ==-1){
+            System.out.println("Element not found in the array");
+        }else{
+            System.out.println("Element found at index " + index);
+        }
     }
 }
